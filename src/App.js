26e52dec1d149c0b0604f0194176d2d47988db5e
Vivek <vkmm777@gmail.com>
2025-01-12 19:21:11 +0530

    add nav and setup route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Company</h1>}></Route>
+          <Route path="/add" element={<h1>Add new product</h1>}></Route>
+          <Route path="/update" element={<h1>Update Product Details</h1>}></Route>
+          <Route path="/logout" element={<h1>Logout</h1>}></Route>
+          <Route path="/profile" element={<h1>Company Profile</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
