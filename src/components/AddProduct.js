7fcdfe69ca655f6146a6cb294a2609563f8e0af9
Vivek <vkmm777@gmail.com>
2025-01-23 08:35:38 +0530

    integrated add product api

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -3,11 +3,25 @@ import React, {useState} from 'react';
 const AddProduct = () => {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
-    const [product, setProduct] = useState('');
+    const [category, setCategory] = useState('');
     const [company, setCompany] = useState('');
 
-    const onAddProduct = () => {
-        console.log(name, price, product, company);
+    const onAddProduct = async () => {
+        const userId = JSON.parse(localStorage.getItem('user'))._id;
+        if(name && price && category && company && userId) {
+            let apiResponse = await fetch("http://localhost:5000/add-product", {
+                method: 'post',
+                body: JSON.stringify({name, price, category, company, userId}),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            apiResponse = await apiResponse.json();
+            if(apiResponse?.result) {
+                
+            }   
+        } else {
+        }
     }
 
     return (
@@ -15,7 +29,7 @@ const AddProduct = () => {
             <h1>Add Product</h1>
             <input type="text" placeholder="Enter Product name" className='input-box' value={name} onChange={(e) => setName(e.target.value)} />
             <input type="text" placeholder="Enter Product price" className='input-box' value={price} onChange={(e) => setPrice(e.target.value)} />
-            <input type="text" placeholder="Enter Product category" className='input-box' value={product} onChange={(e) => setProduct(e.target.value)} />
+            <input type="text" placeholder="Enter Product category" className='input-box' value={category} onChange={(e) => setCategory(e.target.value)} />
             <input type="text" placeholder="Enter Product company" className='input-box' value={company} onChange={(e) => setCompany(e.target.value)} />
             <button className='app-button' onClick={onAddProduct}>Add Product</button>
         </div>
