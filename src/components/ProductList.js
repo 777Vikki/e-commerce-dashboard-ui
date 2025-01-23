5f307e2ceef059f18c306f704b6d6caa4286778e
Vivek <vkmm777@gmail.com>
2025-01-23 08:55:37 +0530

    delete api integrated

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -17,24 +17,46 @@ const ProductList = () => {
             }
         }
     }
+
+    const onDeleteProduct = async (product) => {
+        const productId = product._id;
+        if(productId) {
+            let apiResponse = await fetch('http://localhost:5000/product/' + productId, {
+                method: 'Delete'
+            });
+            apiResponse = await apiResponse.json();
+            if(apiResponse?.result) {
+                getProductList();
+            }
+        }
+    };
+
     return (
         <div className="product-list">
-            <h3>Product List</h3>
-            <ul>
-                <li>S.No.</li>
-                <li>Name</li>
-                <li>Price</li>
-                <li>Category</li>
-            </ul>
             {
-                products.map((product, index) => 
-                    <ul key={index}>
-                        <li>{index + 1}</li>
-                        <li>{product.name}</li>
-                        <li>$ {product.price}</li>
-                        <li>{product.category}</li>
+                products?.length === 0? <div>No Product Added</div> 
+                :
+                <>
+                    <h3>Product List</h3>
+                    <ul>
+                        <li>S.No.</li>
+                        <li>Name</li>
+                        <li>Price</li>
+                        <li>Category</li>
+                        <li>Operation</li>
                     </ul>
-                )
+                    {
+                        products.map((product, index) => 
+                            <ul key={index}>
+                                <li>{index + 1}</li>
+                                <li>{product.name}</li>
+                                <li>$ {product.price}</li>
+                                <li>{product.category}</li>
+                                <li><button onClick={() => onDeleteProduct(product)}>Delete</button></li>
+                            </ul>
+                        )
+                    }
+                </>
             }
         </div>
     )
